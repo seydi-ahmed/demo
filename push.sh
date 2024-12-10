508dd844d364806cd78bb26752a5b98c1af8212c
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-12-10 16:33:36 +0000

    dependance

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "first test"
+git commit -m "dependance"
 git push
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, Spring Boot!";
+        return "Hello!";
     }
 }
