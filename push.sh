0520eda5d40778c860cfd257ea002268d4126069
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-12-15 08:36:23 +0000

    Readme 3

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Readme 2"
+git commit -m "Readme 3"
 git push
